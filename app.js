f369e2bade7c0e95cc1475390606ec62ce745c40
Eddie Lin <yshlin@gmail.com>
2016-09-22 18:03:43 +0800

    Change API path.

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 var request = require('request');
 var config = require('./config');
 var device = require(`./lib/${config.deviceType}`);
-var POST_API = `http://${config.server}:${config.port}/sensors/` +
-               `${config.sensorId}/data?apiKey=${config.apiKey}`;
+var POST_API = `http://${config.server}:${config.port}/pm25/station/` +
+               `${config.sensorId}/data?api_key=${config.apiKey}`;
 
 device.on('data', function(data) {
   request.post(POST_API, {
-    form: { pm10: data.pm10, pm25: data.pm25, pm100: data.pm100 }
+    form: { pm1: data.pm1, pm2_5: data.pm2_5, pm10: data.pm10 }
   }, function(err, response, body) {
     !err ? console.log(body) : console.error(err);
   });
-  console.log(`PM1.0: ${data.pm10}\nPM2.5: ${data.pm25}\nPM10: ${data.pm100}`);
+  console.log(`PM1.0: ${data.pm1}\nPM2.5: ${data.pm2_5}\nPM10: ${data.pm10}`);
 });
--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 'use strict';
 
 module.exports = {
+  //server: 'api.sensorweb.io',
   server: 'localhost',
-  port: 8081,
+  port: 80,
   // Device Type: `arduino`, `linkit`, `usbttl`, and `simulator`.
-  deviceType: 'simulator',
-  sensorId: 'sensorId',
-  apiKey: 'apiKey',
+  deviceType: 'usbttl',
+  sensorId: 'Sy8a912X',
+  apiKey: '9fb2e2537d2354d63bd9d18b4e132f7a9b3cc4386cd02ab87d67058fd4fea746',
   // The simulator options.
   simulator: {
     samplingRate: 5000,
     dataRange: {
-      pm10: { min: 1, max: 10 },
-      pm25: { min: 1, max: 10 },
-      pm100: { min: 10, max: 20 }
+      pm1: { min: 1, max: 10 },
+      pm2_5: { min: 1, max: 10 },
+      pm10: { min: 10, max: 20 }
     }
   }
 };
